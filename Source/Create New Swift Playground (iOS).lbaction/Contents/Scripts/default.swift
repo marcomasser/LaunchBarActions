677f73c2db4a64bceba15ccf5a9d6cd8b8ec329c
Marco Masser <marco@duckcode.com>
2015-08-20 16:35:37 +0200

    Added action for creating iOS playground

---
+++ Source/Create New Swift Playground (iOS).lbaction/Contents/Scripts/default.swift
@@ -0,0 +1,105 @@
+#!/usr/bin/env xcrun swift
+
+import Cocoa
+
+
+func getEnvironment(name: String) -> String {
+    let environment = NSProcessInfo.processInfo().environment
+    if let result: AnyObject = environment[name] {
+        if let resultString = result as? String {
+            return resultString
+        } else {
+            NSLog("Value for environment variable \(name) is not a String")
+        }
+    } else {
+        NSLog("No value for environment variable \(name)")
+    }
+
+    exit(1)
+}
+
+func getTemplatePath() -> String {
+    let actionBundle = NSBundle(path:getEnvironment("LB_ACTION_PATH"))
+    if let actionBundle = actionBundle {
+        let maybeTemplatePath = actionBundle.pathForResource("Template", ofType:"playground")
+        if let templatePath = maybeTemplatePath {
+            if NSFileManager.defaultManager().fileExistsAtPath(templatePath) {
+                return templatePath
+            }
+            NSLog("Template file doesn't exist: \(templatePath)")
+        } else {
+            NSLog("Template.playground not found in action's bundle")
+        }
+    } else {
+        NSLog("Unable to find action bundle")
+    }
+
+    exit(1)
+}
+
+func getDestinationDirectory() -> String {
+    let preferencesPath = getEnvironment("LB_SUPPORT_PATH").stringByAppendingPathComponent("preferences.plist")
+    if !NSFileManager.defaultManager().fileExistsAtPath(preferencesPath) {
+        let documentDirectories = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        if var result = documentDirectories.first as? NSString {
+            result = result.stringByAbbreviatingWithTildeInPath
+            (["destinationDirectory": result] as NSDictionary).writeToFile(preferencesPath, atomically: true)
+        }
+    }
+
+    let preferences = NSDictionary(contentsOfFile:preferencesPath)
+    if let preferences = preferences {
+        if let result = preferences["destinationDirectory"] as? String {
+            return result.stringByExpandingTildeInPath
+        }
+    } else {
+        NSLog("Unable to read preferences plist")
+    }
+
+    NSLog("Unable to get destination directory")
+    exit(1)
+}
+
+func getPlaygroundName() -> String {
+    var playgroundName: String!
+    if Process.arguments.count == 1 {
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .ShortStyle
+        formatter.timeStyle = .MediumStyle
+        let dateString = formatter.stringFromDate(NSDate())
+        playgroundName = "New Playground (\(dateString))"
+    } else {
+        playgroundName = Process.arguments[1]
+    }
+    playgroundName = playgroundName.stringByReplacingOccurrencesOfString(":", withString: "-")
+    playgroundName = playgroundName.stringByReplacingOccurrencesOfString("/", withString: "-")
+    playgroundName = playgroundName + ".playground"
+    return playgroundName
+}
+
+func getPlaygroundPath() -> String {
+    return getDestinationDirectory().stringByAppendingPathComponent(getPlaygroundName())
+}
+
+func copyItem(sourcePath: String, targetPath: String) {
+    var error: NSError?
+    if !NSFileManager.defaultManager().copyItemAtPath(sourcePath, toPath:targetPath, error:&error) {
+        if let copyError = error {
+            NSLog("Error copying template playground: \(copyError)")
+            exit(1)
+        }
+    }
+}
+
+
+let playgroundPath = getPlaygroundPath()
+if !NSFileManager.defaultManager().fileExistsAtPath(playgroundPath) {
+    copyItem(getTemplatePath(), playgroundPath)
+}
+
+let shiftKeyDown = (getEnvironment("LB_OPTION_SHIFT_KEY") as NSString).boolValue
+if shiftKeyDown {
+    println(playgroundPath)
+} else {
+    NSWorkspace.sharedWorkspace().openFile(playgroundPath)
+}
